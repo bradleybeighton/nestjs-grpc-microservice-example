5ddd8091c75430ee240f2f9db19e9ce93325978e
Bradley Beighton <brad.beighton@gmail.com>
2023-09-08 21:15:34 +0100

    removed field mask

--- apps/auth-api/src/app/app.module.ts
+++ apps/auth-api/src/app/app.module.ts
@@ -6,17 +6,19 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 
 @Module({
-  imports: [ClientsModule.register([
-    {
-      name: 'AUTH_PACKAGE',
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_PACKAGE',
         transport: Transport.GRPC,
         options: {
           package: 'auth',
           protoPath: join(__dirname, '/protos/auth/auth.proto'),
           url: process.env.AUTH_SERVICE_URL ?? '0.0.0.0:6001',
         },
-    },
-  ]),],
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
